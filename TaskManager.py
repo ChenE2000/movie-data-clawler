6f7afb0d9898b25f93fd8edc8d82309b6ec8af82
chenE2000 <396909028@qq.com>
2023-04-21 15:39:18 +0800

    示例文件上传

--- TaskManager.py
+++ TaskManager.py
@@ -1,5 +1,18 @@
+import time
 from Celebrity import Celebrity
+from Clawler.DouBan import get_movie_id_by_title as get_movie_id_by_title_DouBan
+from Clawler.MaoYan import get_movie_id_by_title as get_movie_id_by_title_MaoYan
+from Clawler.Infrastructure import generate_movie_titles_todo_list
 
 if __name__ == "__main__":
-    Celebrity("2742").clawler_action()
-    Celebrity("28625").clawler_action()
+    
+    # generate movie titles todo list
+    movie_titles_todo_list = generate_movie_titles_todo_list()
+    
+    for movie in movie_titles_todo_list[0:3]:
+        movie.id_DouBan = get_movie_id_by_title_DouBan(movie.title)
+        movie.id_MaoYan = get_movie_id_by_title_MaoYan(movie.title)
+        movie.save_to_json()
+        time.sleep(2)
+
+    
