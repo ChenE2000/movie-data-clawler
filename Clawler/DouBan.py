98b0fa4ef2e75696a0d1184a74c834bda69049d9
chenE2000 <396909028@qq.com>
2023-05-03 11:45:57 +0800

    豆瓣：获取电影基本信息

--- Clawler/DouBan.py
+++ Clawler/DouBan.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 from Clawler.Driver import webDriver
 from tools.TitleFormatter import find_closest_match
+import requests
 import time
+from bs4 import BeautifulSoup
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+}
 
 def get_movie_id_by_title(title: str) -> str:
     """ 通过电影标题获取电影ID """
@@ -33,4 +38,20 @@ def get_movie_id_by_title(title: str) -> str:
     # print(titles[idx])
     return titles[idx]["id"]
 
-
+def get_basic_info_by_id(id):
+    url = f"https://movie.douban.com/subject/{id}/"
+    print(url)
+    resp = requests.get(url, headers=headers)
+    # find div with id="info" using bs4
+    soup = BeautifulSoup(resp.text, "html.parser")
+    info = soup.find_all("div", id="info")[0]
+    print(info.text)
+    pairs = info.text.split("\n")
+    pairs = list(map(lambda x: x.split(":"), pairs))
+    # 去空值
+    pairs = list(filter(lambda x: len(x) == 2, pairs))
+    # 去空格
+    pairs = list(map(lambda x: [x[0].strip(), x[1].strip()], pairs))
+    # 转为字典
+    pairs = dict(pairs)
+    return pairs
