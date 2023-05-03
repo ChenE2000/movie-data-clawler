df44891e4f9da5a1a4ed0e672249ad344ead618f
chenE2000 <396909028@qq.com>
2023-05-03 16:09:07 +0800

    豆瓣：爬取评分&感兴趣数据

--- Clawler/DouBan.py
+++ Clawler/DouBan.py
@@ -38,14 +38,16 @@ def get_movie_id_by_title(title: str) -> str:
     # print(titles[idx])
     return titles[idx]["id"]
 
-def get_basic_info_by_id(id):
+def get_movie_subject_by_id(id) -> BeautifulSoup:
     url = f"https://movie.douban.com/subject/{id}/"
     print(url)
     resp = requests.get(url, headers=headers)
     # find div with id="info" using bs4
-    soup = BeautifulSoup(resp.text, "html.parser")
+    return BeautifulSoup(resp.text, "html.parser")
+
+def get_movie_basic_info_by_subject(soup: BeautifulSoup):
+    """ 通过电影subject页面获取电影基本信息 """
     info = soup.find_all("div", id="info")[0]
-    print(info.text)
     pairs = info.text.split("\n")
     pairs = list(map(lambda x: x.split(":"), pairs))
     # 去空值
@@ -55,3 +57,29 @@ def get_basic_info_by_id(id):
     # 转为字典
     pairs = dict(pairs)
     return pairs
+
+def get_movie_rating_info_by_subject(soup: BeautifulSoup):
+    """ 通过电影subject页面获取电影评分信息 """
+    rating = soup.find_all("strong", class_="ll rating_num")[0].text
+    # find <span> with property="v:votes"
+    votes = soup.find_all("span", property="v:votes")[0].text
+    return {
+        "评分": float(rating),
+        "评分人数": int(votes),
+    }
+
+def get_movie_interested_info_by_subject(soup: BeautifulSoup):
+    """ 通过电影subject页面获取电影感兴趣信息 """
+    # find div with class="subject-others-interests-ft"
+    interested = soup.find_all("div", class_="subject-others-interests-ft")[0]
+    # find all a tags
+    interested = interested.find_all("a")
+    # extract text
+    interested = list(map(lambda x: x.text, interested))
+    # extract number
+    interested = list(map(lambda x: int(x.split("人")[0]), interested))
+    return {
+        "看过": interested[0],
+        "想看": interested[1],
+    }
+    
