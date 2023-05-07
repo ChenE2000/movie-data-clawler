7f0f9cf308a32a37094ad0fe1bbcf21fc639756f
chenE2000 <396909028@qq.com>
2023-05-07 15:26:10 +0800

    完成初始化任务清单功能

--- Clawler/Infrastructure.py
+++ Clawler/Infrastructure.py
@@ -1,15 +1,24 @@
+import os
 import json
 import typing
 from Movie import Movie
 
 def generate_movie_titles_todo_list() -> typing.List[Movie]:
     """ 生成电影标题待爬取列表,以时光网数据为基准 """
+    existed = os.listdir('./metadata/merged/')
+    
     batch = []
     with open('./metadata/ShiGuang/10_000_movies.json', 'r', encoding='utf-8') as f:
         movies = json.load(f)
         for item in movies:
+            if item["name"] + ".json" in existed:
+                # skip existed movies
+                continue
             movie = Movie(title=item["name"])
             movie.id_ShiGuang = item["movieId"]
             batch.append(movie)
 
+    if len(batch) == 0:
+        raise Exception("No movie titles to crawl")
+    
     return batch
