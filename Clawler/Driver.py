82786e6ebf53d7b64e9d77e6c04f17443e5e3586
chenE2000 <396909028@qq.com>
2023-05-07 15:26:35 +0800

    WebDriver：添加无头模式

--- Clawler/Driver.py
+++ Clawler/Driver.py
@@ -1,3 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# TODO 根据参数返回不同的webdriver
 
-webDriver = webdriver.Chrome()
+
+# 设置Chrome选项
+chrome_options = Options()
+chrome_options.add_argument('--headless') # 开启无头模式
+
+
+webDriver = webdriver.Chrome(options=chrome_options)
+
+# webDriver = webdriver.Chrome()
