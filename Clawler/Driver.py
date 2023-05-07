314a83fb19656b44f9b102fbf6a47824c966940b
chenE2000 <396909028@qq.com>
2023-05-07 21:35:04 +0800

    webdriver无头模式完善参数

--- Clawler/Driver.py
+++ Clawler/Driver.py
@@ -1,13 +1,26 @@
-from selenium import webdriver
+import argparse
+from selenium import webdriver as wb
 from selenium.webdriver.chrome.options import Options
-# TODO 根据参数返回不同的webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+# 根据参数返回不同的webdriver
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', type=str, default='local', help='local or remote')
 
 # 设置Chrome选项
 chrome_options = Options()
 chrome_options.add_argument('--headless') # 开启无头模式
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=1920,1080");
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'")
 
-
-webDriver = webdriver.Chrome(options=chrome_options)
-
-# webDriver = webdriver.Chrome()
+args = parser.parse_args()
+if args.mode == "remote":
+    webDriver = wb.Remote(
+        command_executor="http://chrome:4444/wd/hub",
+        desired_capabilities=DesiredCapabilities.CHROME,
+        options=chrome_options
+    )
+else:
+    webDriver = wb.Chrome(options=chrome_options)
